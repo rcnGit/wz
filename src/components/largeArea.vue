05ae96cf82594fc7c27d3a58f23122c7c7a9fb9a
renchaonan <2402868110@qq.com>
2019-08-20 14:18:52 +0800

    大区首页

--- src/components/largeArea.vue
+++ src/components/largeArea.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="largeArea">
-        <div class="wCard">
+        <div class="wCard" v-show="wCardIf">
             <span class="wName">张颖</span><span class="wDt">{{userId}}</span><img src="./img/towDetail.png" class="towDetail"/>
         </div><!--wCard--><!---->
-       
+       <div class="cover_ma" v-show="cover_maIf" @click="closeCoverMa">
+           <div class="maBox">
+               <img src="./img/lin/ma.png"/>
+               <p>微信搜索“大唐财富服务号”</p>
+           </div>
+       </div>
         <div class="areaTopBanner">
              <!-- <img :src="imageURL" class="areaBanner"/> -->
             <img src="./img/areaBg.png" class="topBanner"/>
@@ -13,6 +18,14 @@
             <div class="areaIntr line5" v-html="areaSynopsis"></div>
             <div class="areaMapBox">
                 <img src="./img/map.png" class="map"/>
+                <div class="location locationZ">
+                    <!-- 浙江大区 -->
+                    <div class="locOne locOne1"  @click="tolocation($event,'1')"><img src="./img/locationChoose.png" class="locImg locImgChoose" v-show="true"/><img src="./img/location.png" class="locImg" v-show="false"/>杭州体验中心</div>
+                    <div class="locOne locOne2"  @click="tolocation($event,'2')"><img src="./img/locationChoose.png" class="locImg locImgChoose" v-show="false"/><img src="./img/location.png" class="locImg" v-show="true"/>金华体验中心</div>
+                    <div class="locOne locOne3"  @click="tolocation($event,'3')"><img src="./img/locationChoose.png" class="locImg locImgChoose" v-show="false"/><img src="./img/location.png" class="locImg" v-show="true"/>宁波体验中心</div>
+                    <div class="locOne locOne4"  @click="tolocation($event,'4')"><img src="./img/locationChoose.png" class="locImg locImgChoose" v-show="false"/><img src="./img/location.png" class="locImg" v-show="true"/>温州体验中心</div>
+                    <div class="locOne locOne5"  @click="tolocation($event,'5')"><img src="./img/locationChoose.png" class="locImg locImgChoose" v-show="false"/><img src="./img/location.png" class="locImg" v-show="true"/>义乌体验中心</div>
+                </div>
                  <div class="expCenter">
                     <!-- <div class="expOne">杭州一部体验中心</div><div class="expOne">杭州一部体验中心</div><div class="expOne">杭州一部体验中心</div> -->
                     <div class="expOne" v-for="item in centerList" @click="jumpExp(item.groupId)">{{item.centerName}}</div>
@@ -36,29 +49,19 @@
                         </mt-swipe>
                 </div>
             </div>
-        <div class="activeBox pl26 pr26">
-            <div class="areaTitle"><img src="./img/areaTitleBg.png" class="areaTitleBg"/><span class="titsp">尊享活动</span><img src="./img/more.png" class="areaMore"/></div>
-            <div class="activeOne">
-                <img src="./img/lin/active.png" class="activeImg"/>
-                <div class="activeDetail">
-                    <div class="tit">家族信托业务新业务分享</div>
-                    <div class="time">活动时间：<span>2018-12-06至2018-12-06</span></div>
-                    <div class="address">活动地点：<span>浙江省杭州市江干区钱江路1366号华润大厦B座2508</span></div>
-                </div>
-                 <div style="clear:both;"></div>
-            </div><!--activeOne-->
-           
-            <div class="activeOne" style="margin-top:0;">
-                <img src="./img/lin/active.png" class="activeImg"/>
-                <div class="activeDetail">
-                    <div class="tit">家族信托业务新业务分享</div>
-                    <div class="time">活动时间：<span>2018-12-06至2018-12-06</span></div>
-                    <div class="address">活动地点：<span>浙江省杭州市江干区钱江路1366号华润大厦B座2508</span></div>
+        <div class="activeBox pl26 pr26" v-show='activeShow'>
+            <div class="areaTitle"><img src="./img/areaTitleBg.png" class="areaTitleBg"/><span class="titsp">尊享活动</span><img src="./img/more.png" class="areaMore" v-if="activeMore" @click="openActiveList()"/></div>
+            <div class="activeOne" v-for="(item,index) in actList" v-if='index<=1' @click="openActiveDetail(item.oaActId)">
+                <img :src="item.bulletinPicture" class="activeImg"/>
+                <div class="activeDetail" :oaActId="item.oaActId">
+                    <div class="tit">{{item.actName}}</div>
+                    <div class="time">活动时间：<span>{{subtext(item.beginTime,10)}}至{{subtext(item.endTime,10)}}</span></div>
+                    <div class="address">活动地点：<span>{{item.location}}</span></div>
                 </div>
                  <div style="clear:both;"></div>
             </div><!--activeOne-->
         </div><!--activeBox-->
-        <div class="space"></div>
+        <div class="space" v-show='activeShow'></div>
         <div class="wealth pl26 pr26">
             <div class="areaTitle"><img src="./img/areaTitleBg.png" class="areaTitleBg"/><span class="titsp">尊享财富</span></div>
             <div class="wealthTop"><span>立足高端客户</span><span>|</span><span>全市场精挑细选</span><span>|</span><span>综合金融服务平台</span></div>
@@ -88,17 +91,17 @@
         <div class="knowUs pl26 pr26">
             <div class="areaTitle"><img src="./img/areaTitleBg.png" class="areaTitleBg"/><span class="titsp">了解大唐</span></div>
             <div class="knowUsMain">
-                <div class="knowUsOne">
+                <div class="knowUsOne" @click="open('pingtai')">
                     <img src="./img/knowUs1.png" class="knowUsImg"/>
-                    <p class="p1">公司简介</p>
+                    <p class="p1">平台简介</p>
                     <p class="p2">专业财富管理公司</p>
                 </div>
-                <div class="knowUsOne">
+                <div class="knowUsOne" @click="open('events')">
                     <img src="./img/knowUs2.png" class="knowUsImg"/>
                     <p class="p1">大事记</p>
                     <p class="p2">公司荣誉</p>
                 </div>
-                <div class="knowUsOne">
+                <div class="knowUsOne" @click="open('safety')">
                     <img src="./img/knowUs3.png" class="knowUsImg"/>
                     <p class="p1">安全保障</p>
                     <p class="p2">合规运作7年</p>
@@ -108,11 +111,11 @@
         </div><!--knowUs-->
         <div class="space"></div>
         <div class="qrCodeBox pl26 pr26">
-            <div class="qrCodeOne qrCodeOne1">
+            <div class="qrCodeOne qrCodeOne1" @click="download()">
                 <img src="./img/lin/ma.png" class="qrCode"/>
                 <p class="qrCodeText">下载大唐财富APP</p>
             </div>
-            <div class="qrCodeOne" >
+            <div class="qrCodeOne" @click="showMa">
                 <img src="./img/lin/ma.png" class="qrCode"/>
                 <p class="qrCodeText">关注微信服务号</p>
             </div>
@@ -129,14 +132,32 @@ export default {
     name:"largeArea",
     data:function(){
         return{
+            wCardIf:'',//是否显示财富师名片
             userId: '',
             type:'',
             bannerList: [],
             areaName:'',//大区名称,
             imageURL:'',
+            actStatus:'1',//活动状态状态 0即将举办 1举办中 2已举办
+            actList:'',//活动列表
+            activeShow:true,//是否显示活动
+            activeMore:'true',//活动更多按钮
+            client:'',//终端
             areaSynopsis:'',//大区概述,
+            cover_maIf:false,//放大的服务号二维码是否显示
             baseImg:'./img/areaBg.png',
-            centerList:[],
+            centerList:'',
+            centerListZ1:[{'centerName':'直属体验中心','groupId':''},{'centerName':'杭州一部体验中心','groupId':''},{'centerName':'杭州二部体验中心','groupId':''}],
+            centerListZ2:[{'centerName':'金华体验中心','groupId':''}],
+            centerListZ3:[{'centerName':'宁波三部体验中心','groupId':''},{'centerName':'宁波四部体验中心','groupId':''}],
+            centerListZ4:[{'centerName':' 温州一部体验中心','groupId':''},{'centerName':' 温州二部体验中心','groupId':''}],
+            centerListZ5:[{'centerName':'义乌体验中心','groupId':''}],
+            centerListX1:[{'centerName':'成都一部体验中心','groupId':''},{'centerName':'成都二部体验中心','groupId':''},{'centerName':'成都三部体验中心','groupId':''}],
+            centerListX2:[{'centerName':'贵阳体验中心','groupId':''}],
+            centerListX3:[{'centerName':' 昆明一部体验中心','groupId':''},{'centerName':'昆明二部体验中心','groupId':''}],
+            centerListX4:[{'centerName':' 南宁体验中心','groupId':''}],
+            centerListX5:[{'centerName':'玉溪体验中心','groupId':''}],
+            centerListX6:[{'centerName':'重庆一部体验中心','groupId':''},{'centerName':'重庆二部体验中心','groupId':''}],
         }
     },
     methods:{
@@ -148,6 +169,85 @@ export default {
                 slidesOffsetAfter : 30,//距离右边边30px
                 //slidesOffsetBefore : 100,//距离左边距离
                 })     
+        },
+        subtext:function(t){
+            if(!t==false){
+                return t.substr(0,10)
+            }else{
+                return '';
+            }
+
+        },
+        open:function(name){
+             this.$router.push({
+                path:'/'+name,
+                name:name,
+                query:{
+                    //comefrom:this.param.comefrom,//是否糖罐进入
+                }
+            })
+        },
+        closeCoverMa:function(){
+            this.cover_maIf=false;//隐藏放大的二维码；
+        },
+        showMa:function(){
+            this.cover_maIf=true;//显示放大的二维码；
+        },
+        download:function(){//下载大唐财富师App
+            var browser = {
+	            versions: function() {
+	                var u = navigator.userAgent,
+	                    app = navigator.appVersion;
+	                return { //移动终端浏览器版本信息   
+	                    ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端  
+	                    android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器  
+	                    iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者QQHD浏览器  
+	                };
+	            }(),      
+	            language: (navigator.browserLanguage || navigator.language).toLowerCase()
+	        }
+            var android = browser.versions.android;
+            var iOS = browser.versions.ios;
+
+            /*唤起APP的方法*/
+            if(iOS){ 
+                    window.location='tangyuanbaodatang://platformapi/startapp?param='
+                    var clickedAt = +new Date;  
+                    setTimeout(function(){
+                        !window.document.webkitHidden && setTimeout(function(){ 
+                            if (+new Date - clickedAt < 2000){  
+                                window.location = 'https://interface.tdyhfund.com/launcher/download.html?channel=app&name=dtcf';  
+                            }  
+                        }, 500);       
+                    }, 2500)
+            }else if(android){
+                window.location='tangyuanbaodatang://platformapi/startapp?param='+str
+                var clickedAt = +new Date;  
+                    setTimeout(function(){
+                        !window.document.webkitHidden && setTimeout(function(){ 
+                            if (+new Date - clickedAt < 2000){  
+                                window.location = 'https://interface.tdyhfund.com/launcher/download.html?channel=app&name=dtcf';  
+                            }  
+                        }, 500);       
+                    }, 2500)
+                }
+    
+    
+        },
+        openActiveDetail:function(id,n){
+            var actName=encodeURIComponent(n);
+            if(this.client == 'IOS' || this.client == 'Android'|| this.client == '0'){//大唐财富师APP
+                window.location.href='https://weixin-test-interface.tdyhfund.com/weixin-h5/index.html#/activeDetail?actId='+id+'&actName='+actName+'&comefrom=tangguan';
+            }else{
+                window.location.href='https://weixin-test-interface.tdyhfund.com/weixin-h5/index.html#/activeDetail?actId='+id+'&actName='+actName+'&comefrom=tangguan';
+            }
+        },
+        openActiveList:function(){
+             if(this.client == 'IOS' || this.client == 'Android'|| this.client == '0'){//大唐财富师APP
+                window.location.href='https://weixin-test-interface.tdyhfund.com/weixin-h5/index.html#/active?areaId='+this.areaId+'&comefrom=tangguan';
+            }else{
+                window.location.href='https://weixin-test-interface.tdyhfund.com/weixin-h5/index.html#/active?areaId='+this.areaId;
+            }
         },
         getBanner:function(){//获取广告配置
             let that = this;
@@ -165,7 +265,6 @@ export default {
                 Indicator.close();
                 var data=Base64.decode(res.data);
                 data=jQuery.parseJSON(data);
-                console.log(data)
                 var retCode=data.retCode;
                 var retMsg=data.retMsg;
                 if(retCode == 0){
@@ -179,7 +278,7 @@ export default {
             let that = this;
             Indicator.open();
             that.areaId='35f20843e84b4aeba18e689b2591d1ed'
-            var param=Base64.encode('{"userId":"'+that.userId+'","areaId":"'+that.areaId+'"}');//that.user.userId
+            var param=Base64.encode('{"areaId":"'+that.areaId+'"}');//that.user.userId
             axios({
                 method:'get',
                 url:'/olmgweb/wzApiController/getAreaAndCenterInfo',//获取大区信息及其下属体验中心信息
@@ -199,12 +298,81 @@ export default {
                     that.areaName=data.areaName//大区名称,
                     that.imageURL=data.imageURL
                     that.areaSynopsis=data.areaSynopsis//大区概述,
-                    that.centerList=data.centerList
+                    // that.centerList=data.centerList
                 }
             })
         },
+        getActive:function(){//获取活动
+            let that = this;
+            Indicator.open();
+            var param={"pageNo":"1","actStatus":that.actStatus}//that.user.userId
+            axios({
+                method:'get',
+                url:'/wei/wxservice/wxexternal?opName=getactiveinfo',//获取活动列表
+                params:{
+                    param:param,
+                }
+            })
+            .then(function(res) {//成功之后
+                Indicator.close();
+                 console.log(res.data)
+                var itemList=res.data.itemList;
+               if(itemList.length==0){
+                   if(that.actStatus==1){//状态 0即将举办 1举办中 2已举办
+                       that.actStatus=0;
+                       that.getActive();
+                        return;
+                   }else if(that.actStatus==0){
+                       that.actStatus=2;
+                       that.getActive();
+                       return;
+                   }else{
+                       //隐藏该模块
+                       that.activeShow=false;
+                   }
+               }else if(itemList.length<=2){
+                that.activeMore=false;//不显示更多活动按钮
+                that.actList=itemList;
+               }else{
+                that.activeMore=true;//显示更多活动按钮
+                that.actList=itemList;
+               }
+                
+            })
+        },
         jumpExp:function(id){
-            console.log(id)
+            console.log(id);
+            this.$router.push({
+                path:'/expCenterIndex',
+                name:'expCenterIndex',
+                query:{
+                    groupId:id,//是否糖罐进入
+                }
+            })
+        },
+        tolocation:function(e,z){
+            $('.locOne').find('.locImg').show();
+             $('.locOne').find('.locImgChoose').hide();
+             // e.currentTarget 是你绑定事件的元素
+             var obj=e.currentTarget;
+             $(obj).find('.locImg').hide();
+            $(obj).find('.locImgChoose').show();
+            
+            console.log($(this).find('.locImgChoose').length)
+            if(z==1){
+                this.centerList=this.centerListZ1;
+            }else if(z==2){
+                this.centerList=this.centerListZ2;
+            }else if(z==3){
+
+                this.centerList=this.centerListZ3;
+                console.log(this.centerListZ3);
+            }else if(z==4){
+                this.centerList=this.centerListZ4;
+            }else if(z==5){
+                this.centerList=this.centerListZ5;
+            }
+           
         }
     },
     components:{Swipe, SwipeItem,Swiper},
@@ -214,15 +382,83 @@ export default {
     created:function(){
         this.userId = this.$route.query.userId;
         this.areaId = this.$route.query.areaId;
-        this.userId='DT1603225'
-        this.getBanner()
-        this.getAreaInfo()
+        this.centerList=this.centerListZ1;//
+        this.userId='DT1603225';
+        this.client=this.$route.query.client;
+        //this.getBanner();
+        //this.getAreaInfo();
+        //this.getActive()//获取活动列表；
     }
 }
 </script>
 
 <style>
     @import 'area.css';
+    .location{
+        position: absolute;
+        top:0;
+        left:0;
+        width:100%;
+        height:100%;
+        /* background:red; */
+    }
+    .locOne{
+        position: absolute;
+        font-size: .293333rem;
+        color:#000;
+    }
+    .locationZ .locOne1{
+        top: 26%;
+        left: 33%;
+    }
+    .locationZ .locOne2{
+        top: 37%;
+        left: 32%;
+    }
+    .locationZ .locOne3{
+        top: 29%;
+        left: 64%;
+    }
+    .locationZ .locOne4{
+        top: 60%;
+        left: 41%;
+    }
+    .locationZ .locOne5{
+        top: 44%;
+        left: 39%;
+    }
+    .locationZ .locImg{
+        width:.506667rem;
+        vertical-align: top;
+        padding-right: 0.1rem;
+    }
+    .cover_ma{
+        width:100%;
+        height:100%;
+        position: fixed;
+        background-color: rgba(0,0,0,0.6);
+        z-index: 99;
+        top:0;
+        left:0;
+    }
+    .maBox{
+        width:4rem;
+        height:5rem;
+        position: absolute;
+        top:50%;
+        left:50%;
+        margin-top:-2.5rem;
+        margin-left:-2rem;
+    }
+    .maBox p{
+        font-size:.32rem;
+        margin-top:.2rem;
+        text-align: center;
+        color:#fff;
+    }
+    .maBox img{
+        width:100%;
+    }
 </style>
 
 
